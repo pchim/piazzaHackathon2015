d2a596b09478ddf726c0135d97c6541b995de020
ibarreto <iabarreto11@gmail.com>
2015-08-08 09:46:14 -0700

    initial scaffold

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('piazzahackApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
