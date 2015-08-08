7cb3ff596315250b3dbe550515f30ff5c8da5d31
Annie Chen <annieche@andrew.cmu.edu>
2015-08-08 13:54:04 -0700

    Functions for participants and comments

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -6,7 +6,7 @@ angular.module('piazzahackApp')
     $scope.init = function(){
       $scope.courses = [];
       $scope.user = [];
-      $scope.currentCourse='';
+      $scope.course='';
 
       $http.get('/api/courses')
         .success(function(data) {
@@ -43,7 +43,7 @@ angular.module('piazzahackApp')
       }
     };
 
-    $scope.createSession = function(index){
+    $scope.createSession = function(course){
       $scope.participants = [$scope.user.name]
       $scope.newSessionData={
         date: $scope.date,
@@ -52,10 +52,10 @@ angular.module('piazzahackApp')
         max_number_people: $scope.max_number_people,
         participants: $scope.participants
       }
-      $scope.courses[index].sessions.push(newSessionData);
-      $http.put('/api/courses', $scope.courses[index].sessions)
+      course.sessions.push(newSessionData);
+      $http.put('/api/courses', course.sessions)
         .success(function(data){
-          console.log("sessions", $scope.courses[index].sessions);
+          console.log("sessions", course.sessions);
         })
         .error(function(data){
           console.log('Error: ' + data);
@@ -63,49 +63,69 @@ angular.module('piazzahackApp')
         $scope.newSessionData= {};
     }
 
-    $scope.updateSession = function(couseIndex, index){
+    $scope.updateSession = function(session){
       $scope.updateSessionData={
-        date: $scope.date,
-        location: $scope.studyLocation,
-        message: $scope.sessionName,
-        max_number_people: $scope.max_number_people,
-        participants: $scope.participants,
-        comments: $scope.comments
+        date: session.date,
+        location: session.studyLocation,
+        message: session.sessionName,
+        max_number_people: session.max_number_people,
+        participants: session.participants,
+        comments: session.comments
       }
-      $scope.courses[courseIndex].sessions[index] = $scope.updateSessionData;
-      $http.put('/api/courses', $scope.courses[courseIndex].sessions[index])
+      // session.sessions[index] = $scope.updateSessionData;
+      $http.put('/api/courses', session)
         .success(function(data){
           $scope.updateSessionData={};
-          console.log("sessions", $scope.courses[courseIndex].sessions[index]);
+          console.log("sessions", session);
         })
         .error(function(data){
           console.log('Error: ' + data);
       });
     }
 
-    $scope.addComment = function(courseIndex, index){
+    $scope.addComment = function(session){
       $scope.newComment={
         comment: $scope.commentMessage,
         author: $scope.user.name,
-        date: Date.now()
+        date: Date.now(), 
+        last_modified: null()
       }
-      $scope.courses[courseIndex].sessions[index].comments.push($scope.newComment);
+      session.comments.push($scope.newComment);
+      updateSession(session);
     }
 
+    $scope.editComment = function(session, comment){
+      $scope.editComment={
+        comment: comment.commentMessage, 
+        last_modified: Date.now()
+      }
+      updateSession(session);
+    }
 
-    $scope.deleteSession = function(courseIndex, index){
-      $scope.courses[courseIndex].sessions.splice(index,1);
-      $http.put('/api/courses', $scope.courses[index].sessions)
+    $scope.addParticipant = function(session){
+      session.participants.push($scope.user.name); //will not work with people with same name...?
+    }
+
+    $scope.deleteParticipant = function(session){
+      var index = participants.indexOf($scope.user.name);
+      session.participants.splice(index, 1);
+    }
+
+    $scope.deleteComment = function(session, index){
+      session.comments.splice(index, 1);
+      updateSessions(session);
+    }
+
+    $scope.deleteSession = function(course, index){
+      course.sessions.splice(index,1);
+      $http.put('/api/courses', course.sessions)
         .success(function(data){
-          console.log("sessions", $scope.courses[index].sessions);
+          console.log("sessions", course.sessions);
         })
         .error(function(data){
           console.log('Error: ' + data);
         });
     }
-
-
-
 
     $scope.deleteCourse = function(course) {
       $http.delete('/api/courses/' + course._id)
